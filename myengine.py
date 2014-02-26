ed12d483df021567436761a26116f9f393c2d6cd
Andres Gomez <andres_gomez@ieee.org>
2014-02-26 00:28:55 -0600

    cumple requrimientos proyecto 2

--- myengine.py
+++ myengine.py
@@ -1,4 +1,5 @@
-k#extracts the words from the text content of the file,
+#this program reads one html file (URL predefined),
+#extracts the words from the text content of the file,
 #converts the words into lower case, 
 #writes out the words to an output file.
 
@@ -6,73 +7,77 @@ import urllib2
 from bs4 import *
 from urlparse import urljoin
 import re
+from collections import namedtuple
+import pickle
+# Global array for stop words
+stopwords = []
+nDoc = 0
+invIndex = {}
+docs = {}
 
-global deep_cur
+
 
 #assume pages contain one page only for now
-def crawl(page, outfile, docId, stopwords, ):
-
-    
-    new_links = []
-    try:
-        c = urllib2.urlopen(page)
-    except:
-        page = urljoin(page,"index.html")
+def crawl(pages, outfile, docfile, stopwords, depth):
+    for page in pages:
+        # try to open a URL, if not succeed, 
+        # try again with index.html at the end of the URL
+        
+        depth_Temp = depth;
         try:
-            print "RETRYING.... %s\n" % page
             c = urllib2.urlopen(page)
         except:
-            print "could not open the %s" % page
-            return new_links
+            page = urljoin(page,"index.html")
+            try:
+                print "RETRYING.... %s\n" % page
+                page = urllib2.urlopen(page)
+            except:
+                print "could not open the %s" % page
+                return 0
+        global nDoc 
+        global docs
+        nDoc += 1
+        if not str(page) in docs:
+            docfile.write("%d\t%s\n" % (nDoc, str(page)))
+            docs[str(page)] = nDoc
+        else:
+            return 0
 
-    soup = BeautifulSoup(c.read())
-    
-    print(pages[docId])
+        soup = BeautifulSoup(c.read())
 
-    #call code here to remove the html tags from the html file 
-    #resulting in Unicode u'Hello'
-    # Only add links to craw if the deep < 2
-    if deep_cur <= 2:
-        new_links = GetChildlinks(soup);
-    else:
-        new_links = ["None"]
-     
+        #call code here to remove the html tags from the html file 
+        #resulting in Unicode u'Hello'
+        text = GetTextOnly(soup)
         
-    
-    text = GetTextOnly(soup);
+        i = 0 
+        #separate and stem the words
+        words = SeperateWords( text, stopwords,nDoc)
+        # for w in words:
+        #     i += 1
+        #     outfile.write("%20s\t%s\t%s\n" % (str(w), str(nDoc), str(i)))
 
-    #separate and stem the words
-    words = SeperateWords(text,stopwords)
-    i = 0
-    for w in words:
-        outfile.write("%20s\t%s\t%s\n" % (str(w), str(docId), str(i)))
-        i = i + 1
-    return new_links
-
+        # get the links and crawl over them if deep < 2
+        if depth_Temp < 2:
+            links = GetChildlinks(soup)
+            if len(links) > 0:
+                depth_Temp += 1
+                crawl(links, outfile, docfile, stopwords, depth_Temp)
+                pass
+            pass
         
 #remove all tags, return a list of words (one word per line)
 #from the text content of the html file
 def GetTextOnly(soup):
-    
     v = soup.string
     if v==None:
         c = soup.contents
         resulttext = ''
         for t in c:
             subtext = GetTextOnly(t)
-            # print(subtext)
-            # findNumber = re.compile('^[0-9]+|\s[0-9]+')
-            # if (findNumber.search(subtext) == None):  # the condition is true if w is NOT a number
-                # print(subtext)
             resulttext +=subtext + '\n'
-                
-            
         return resulttext
     else:
         return v.strip()
-
-
-
 
 # Returns all child link from the parent URL
 def GetChildlinks(soup):
@@ -81,19 +86,14 @@ def GetChildlinks(soup):
     
     for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
         links.append(link.get('href'))
-        # pages.append(link.get('href'))
-        # print(len(pages))
-        
-    # print(links)
-    # print(pages)
-
     return links;
 
-def SeperateWords(text,stopwords):
+def SeperateWords(text,stopwords,docID):
     output = []
+    docDict = {}
     splitter = re.compile('[\W][\']|[\'][\W]|[^\'0-9a-zA-Z]')
     words = splitter.split(text)
-    
+    wordPos = 0;
     for s in words:
         if s!='':
             # After separate words checks for terms starting with a number
@@ -101,82 +101,63 @@ def SeperateWords(text,stopwords):
             findNumber = re.compile('^[0-9]+|\s[0-9]+')
             # the condition is true if s does NOT start with a number
             if (findNumber.search(s) == None):
+                #if word is already indexed
                 # Verify stopwords
-                if not s.lower() in stopwords:
-                    output.append(s.lower())
-                    pass
+                s = s.lower()
+                if not s in stopwords:
+                    if not s in docDict:
+                        #if new term, add freq as [0] and add the pos in +[1]
+                        docDict[s] = []
+                        docDict[s] = [docID]
+                        docDict[s].append(1) 
+                        docDict[s].append(wordPos) 
+                    else:
+                        # the word exist in the dictionary
+                        # increment the freq [0] and append the pos
+                        docDict[s][1] += 1
+                        docDict[s].append(wordPos) 
+            wordPos += 1
+
+    # getting both the keys and values at once
+    for term,value in docDict.items():
+        # freq = len(docDict[term])
+        # docDict[term].insert(0,freq)
+        freq = docDict[term][0]
+        if not term in invIndex:
+            invIndex[term] = [];
+            invIndex[term] = [docDict[term][1]]
+            pass
+        else:
+            freqTotal = docDict[term][1]
+            invIndex[term][0] = invIndex[term][0] + freqTotal
+       
+        invIndex[term].append(docDict[term])
+        
+        # print "%s=%s" % (term,invIndex[term])
+
+
+
+
     return output
+
+def loadStopWords(stopFile):
+    for line in stopFile:
+        stopwords.append(line.rstrip())
+
+    pass
 
 if __name__ == '__main__':
         
-    outfile = open("wordlist.txt", "w")
-    pagesFile = open("documents.txt", "w")
-    docId = 0
-    pages = ['http://www.mtsu.edu/~csdept/index.htm']
-    stopwords = []
-    # pages = ['http://man7.org/linux/man-pages/man2/mmap.2.html']
-    root = len(pages)
-    # ensure extensions are suported
-    deep_cur = 0;
-    # open file with stop words and index them in a list
-    f = open('stopwords','r')
-    for line in f:
-        stopwords.append(line.rstrip())
-        
-    f.close()
-
+    outfile = open("posIndex.dat", "w")
+    docfile = open("docs.txt", "w")
     
-    while 1:
-        # Checks of the extension of the link to procced
-        # a = pages[docId].split(".")[-1]
-        pagesFile.write("%s\t%s\n" % (str(docId), str(pages[docId])))
-        for x in xrange(0,len(pages)):
-            if x == docId:
-                print "*\t%s" % pages[x]
-            else:
-                print pages[x]
-            
-        # if (a != "htm") and (a != "html") and (a != "txt"):
-        #     pages[docId] = urljoin(pages[docId],"index.html")
-        # pagesFile.write("%s\t%s\n" % (str(docId), str(pages[docId])))
+    pages = ['http://www.mtsu.edu/~csdept/index.htm']
+    stopwordsFile = open('stopwords','r')
+    stopwordsFile.close()
 
-        new_pages  = crawl(pages[docId],outfile, docId, stopwords)
+    crawl(pages, outfile, docfile, stopwords, 0)
 
-        # print pages[docId]
-        # print (new_pages)
-        for page in new_pages:
-            print "\t%s" % page
-            if not page in pages:
-                pages.append(page)
-                pass
-            else:
-                print "\t\tPage Crawled %s\n" % page
+    pickle.dump(invIndex,outfile)
 
-        # pages = pages + new_pages
-        # print(pages[docId])
-        # checks the current deep in the URL's tree 
-        # if all links in the current level had have been crawl 
-        # increment the deep_cur
-        # print("root")
-        # print(root-docId-1)
-        print ("deep_cur: %d\tDocId: %d\tPages: %d" % (deep_cur, docId, len(pages)))
-        
-        # print("docId")
-        # print(docId)
-        # print("len")
-        # print(len(pages))rm
-        print "---------------------------------------------------------\n"
-
-        if docId >= len(pages)  or deep_cur >= 3:
-            print "EXIT"
-            break
-        if (root-docId) <= 1:
-            root = len(pages)
-            deep_cur = deep_cur + 1
-        docId = docId + 1
-
-                
-
-        
-    pagesFile.close()
     outfile.close()
+    docfile.close()
