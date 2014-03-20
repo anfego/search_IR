c36ae47bee44ff8aa8eaf17f8ded88192d7a6573
Andres Gomez <andres_gomez@ieee.org>
2014-03-19 23:20:46 -0500

    implement query term search

---
+++ queryAnswer.py
@@ -0,0 +1,23 @@
+import pickle
+# Loads the posting Index
+index = open("posIndex.dat", "rb");
+posIndex = pickle.load(index);
+print posIndex['made'];
+
+query =  "Juan made of youtube"
+# query = raw_input('Please enter your query: ');
+
+queryTerms = ' '.join(query.split());
+queryTerms = queryTerms.split(' ');
+k = len(queryTerms);
+print (queryTerms);
+
+i = 0;
+for term in queryTerms:
+	queryTerms[i] = term.lower();
+	if term in posIndex.keys():
+		print "%s -->\t %s\n" % (term, posIndex[term]);
+	else:
+		print "%s -->\n" % (term);
+	i = i +1;
+	
