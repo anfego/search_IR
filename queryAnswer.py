4f9b9c64b2b1e3b1f697f27e4d25eb02e3bfce5d
Andres Gomez <andres_gomez@ieee.org>
2014-03-19 23:38:02 -0500

    added funtion's prototypes

--- queryAnswer.py
+++ queryAnswer.py
@@ -1,23 +1,30 @@
 import pickle
-# Loads the posting Index
-index = open("posIndex.dat", "rb");
-posIndex = pickle.load(index);
-print posIndex['made'];
 
-query =  "Juan made of youtube"
-# query = raw_input('Please enter your query: ');
 
-queryTerms = ' '.join(query.split());
-queryTerms = queryTerms.split(' ');
-k = len(queryTerms);
-print (queryTerms);
+def posInsert(termList_A,termList_B,k):
+	return 0;
 
-i = 0;
-for term in queryTerms:
-	queryTerms[i] = term.lower();
-	if term in posIndex.keys():
-		print "%s -->\t %s\n" % (term, posIndex[term]);
-	else:
-		print "%s -->\n" % (term);
-	i = i +1;
-	
+
+if __name__ == '__main__':
+	# Loads the posting Index
+	index = open("posIndex.dat", "rb");
+	posIndex = pickle.load(index);
+	print posIndex['made'];
+
+	query =  "Juan made of youtube"
+	# query = raw_input('Please enter your query: ');
+
+	queryTerms = ' '.join(query.split());
+	queryTerms = queryTerms.split(' ');
+	k = len(queryTerms);
+	print (queryTerms);
+
+	i = 0;
+	for term in queryTerms:
+		queryTerms[i] = term.lower();
+		if term in posIndex.keys():
+			print "%s -->\t %s\n" % (term, posIndex[term]);
+		else:
+			print "%s -->\n" % (term);
+		i = i +1;
+		
